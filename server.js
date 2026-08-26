ddd01636ddc8c02b4532e476483606363b49bae2
tugalovakobir0-glitch <tugalovakobir0@gmail.com>
2026-08-27 01:10:50 +0900

    fead:yangi paltfoliya publish qildik

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ const express = require("express");
 const app = express();
 const res = require("express/lib/response");
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+
 // 1
 app.use(express.static(`public`));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 // 3
 app.set("views", "views");
 app.set("view engine", "ejs");
@@ -14,6 +26,9 @@ app.set("view engine", "ejs");
 app.post("/create-item", (req, res) => {
   console.log(req.body);
   res.json({ test: `assalomu alaykum hurmatli ` });
+});
+app.get("/develop", (req, res) => {
+  res.render("develop", { user: user });
 });
 app.get("/", function (rep, res) {
   res.render("harid");
